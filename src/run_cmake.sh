78e17e6f3204a9ef515e470426438747781bb486
Rodrigo L. Rosa <rodrigorosa.LG@gmail.com>
2011-09-18 12:42:16 -0700

    comments
    debug/release option

--- src/run_cmake.sh
+++ src/run_cmake.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-(cd build ; cmake ..)
+if [ -z "$1" ]
+then
+    echo "Setting up cmake WITHOUT debug symbols"
+    (cd build ; cmake ..)
+else
+    if [ "$1" == debug ]
+    then
+	echo "Setting up cmake with debug symbols"
+	(cd build ; cmake .. -DCMAKE_BUILD_TYPE=Debug)
+    else
+	echo "Invalid argument"
+    fi
+fi
+     
