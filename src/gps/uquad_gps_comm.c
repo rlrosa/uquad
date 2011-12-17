088ccdd9d6f2cc7cda0a8f79d467a23ee255d458
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2011-12-17 18:03:07 -0200

    config de cmake xa meter las libs de gpsd donde hace falta. hay que compilarlas a mano, pero es una vez, y es fácil (con scons), hay info en submodules/gpsd/
    interfaz bien básica con el gps.
    ahora falta ver bien que es lo que queremos que no de, y probarlo en la beagle.

---
+++ src/gps/uquad_gps_comm.c
@@ -0,0 +1,66 @@
+#include <uquad_gps_comm.h>
+#include <gpsd.h>
+#include <stdlib.h>
+#include <uquad_error_codes.h>
+
+#define GPS_COMM_STREAM_FLAGS_ENA WATCH_ENABLE | WATCH_JSON
+#define GPS_COMM_STREAM_FLAGS_DIS WATCH_DISABLE
+
+gps_t *  gps_comm_init(void){
+    gps_t * gps;
+    int ret;
+    gps = (gps_t *)malloc(sizeof(gps_t));
+    mem_alloc_check(gps);
+
+    // Initialize data structure and open connection
+    ret = gps_open(NULL, 0, gps);
+    if(ret < 0){
+	err_log("GPS init failed, could not open connection to GPS, is daemon running?");
+	free(gps);
+	return NULL;
+    }
+
+    // Start TX from GPS
+    ret = gps_stream(gps, GPS_COMM_STREAM_FLAGS_ENA, NULL);
+    if(ret < 0){
+	err_log("GPS init failed, could not stream from GPS.\nAttempting to close connection...");
+	// deinit, and ignore return value, since gps_comm_deinit takes care of logging.
+	(void) gps_comm_deinit(gps);
+	return NULL;
+    }
+
+    return gps;
+}   
+
+int  gps_comm_deinit(gps_t * gps){
+    int ret;
+    ret = gps_stream(gps, GPS_COMM_STREAM_FLAGS_DIS, NULL);
+    if(ret < 0)
+	err_log("WARNING: ignoring error while terminating GPS stream...");
+    ret = gps_close (gps);
+    if(ret < 0)
+	err_log("WARNING: ignoring error while closing GPS...");
+    free(gps);
+    return ret;
+}
+
+int gps_comm_get_status(gps_t *gps){
+    return gps->status;
+}
+
+int gps_comm_get_fd(gps_t *gps){
+    return gps->gps_fd;
+}
+
+int gps_comm_read(gps_t *gps){
+    int ret;
+    ret = gps_read(gps);
+    if(ret == -1){
+	err_check(ERROR_IO,"New data from expected, but none found...\nWas select(gps_fd) called before gps_comm_read()?");
+    }
+    return ERROR_OK;
+}
+
+struct gps_fix_t gps_comm_get_data(gps_t *gps){
+    return gps->fix;
+}
--- src/test/gps/gps_test.c
+++ src/test/gps/gps_test.c
@@ -1,7 +1,100 @@
-/*
- * A simple command-line exerciser for the library.
- * Not really useful for anything but debugging.
- */
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#ifndef UQUAD_GPS_LIB_TEST // test the uquad interface to gpsd
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+#include <uquad_gps_comm.h>
+#include <uquad_aux_io.h>
+#include <unistd.h>
+
+#define FREE_N_DIE_IF_ERROR(ret,msg) if(ret!=ERROR_OK){fprintf(stderr,"GPS test failed:%s:%d:%s\n",__FILE__,__LINE__,msg);goto kill_n_close;}
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    gps_t *gps;
+    io_t *io;
+    
+    gps = gps_comm_init();
+    if(gps == NULL){
+	err_log("GPS test failed.");
+    }
+
+    io = io_init();
+    if(io == NULL){
+	err_log("GPS test failed.");
+    }
+
+    // add GPS
+    ret = io_add_dev(io, gps_comm_get_fd(gps));
+    FREE_N_DIE_IF_ERROR(ret,"Failed to add device to io.");
+
+    // add stdin to support clean exit
+    ret = io_add_dev(io,STDIN_FILENO);
+    FREE_N_DIE_IF_ERROR(ret,"Failed to add STDIN to dev list");
+
+    /// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+    /// Poll n read loop
+    /// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+    uquad_bool_t read = false,write = false, aux_bool = false;
+    uquad_bool_t reg_gps = true, reg_stdin = true;
+    unsigned char tmp_buff[2];
+    int counter = 0;
+    struct gps_fix_t gps_fix;
+    poll_n_read:
+    while(1){
+        ret = io_poll(io);
+        FREE_N_DIE_IF_ERROR(ret,"io_poll() error");
+        // gps
+        if(reg_gps){
+            ret = io_dev_ready(io,gps_comm_get_fd(gps),&read,&write);
+            FREE_N_DIE_IF_ERROR(ret,"io_dev_ready() error");
+            if(read){
+                ret = gps_comm_read(gps);
+                if(ret != ERROR_OK){
+                    fprintf(stdout,"\nGPS missed frame?\n\n");
+                }else{
+                    ret = gps_comm_read(gps);
+                    if(ret != ERROR_OK){
+                        fprintf(stdout,"\nGPS had no data!\n\n");
+		    }else{
+			//gps_fix = gps_comm_get_data(gps);
+			fprintf(stdout,"%d\n",gps_comm_get_status(gps));
+		    }
+                }
+            }
+        }
+        // stdin
+        if(reg_stdin){
+            ret = io_dev_ready(io,STDIN_FILENO,&read,&write);
+            FREE_N_DIE_IF_ERROR(ret,"io_dev_ready() error");
+            if(read){
+                ret = fread(tmp_buff,1,1,stdin);
+                if(ret<=0)
+                    fprintf(stdout,"\nNo user input!!\n\n");
+                else
+                    // Any user input will terminate program
+                    goto kill_n_close;
+            }
+        }
+        fflush(stdout);
+    }
+
+    // Failure...
+    kill_n_close:
+    ret = io_deinit(io);
+    if(ret != ERROR_OK){
+	err_log("Failed to deinit io.");
+    }
+    ret = gps_comm_deinit(gps);
+    if(ret != ERROR_OK){
+	err_log("Failed to deinit gps.");
+    }
+    return ERROR_OK;
+}
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#else //UQUAD_GPS_LIB_TEST defined, testing gpsd library directly.
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -53,3 +146,5 @@ int main(int argc, char *argv[])
     (void) gps_stream(&gpps_data, WATCH_DISABLE, NULL);
     (void) gps_close (&gpps_data);
 }
+
+#endif //UQUAD_GPS_LIB_TEST
