02bb8b61a5a9adfa68acd04c28bcebc0c816c274
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2011-12-21 19:21:50 -0200

    script que agrega todos los .m solo puse un par de paths, mi idea es ir agregando a medida que haya +.

---
+++ src/uquad_add_path.m
@@ -0,0 +1,13 @@
+% Add paths to all dirs with .m files
+
+uquad_src = pwd;
+if(isunix)
+  slash = '/';
+else
+  slash = '\';
+end
+% GPS support scripts
+addpath(sprintf('%s%cgps',uquad_src,slash));
+% IMU support scripts
+addpath(sprintf('%s%cimu',uquad_src,slash));
+
