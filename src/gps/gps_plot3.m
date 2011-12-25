44d5a07b04ba8a2c0224360888b9d606b1cfaed4
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2011-12-25 13:22:17 -0200

    plotear cada uno por separado

--- src/gps/gps_plot3.m
+++ src/gps/gps_plot3.m
@@ -42,11 +42,16 @@ else
   hold on;
 end
 
+% use relative plot to improve axis resolution
+x = x - mean(x);
+y = y - mean(y);
+z = z - mean(z);
+
 path_color = path_colors(fig_count + 1);
 plot3(x, y, z,sprintf('.-%c',path_color));
-xlabel('Easting (m)')
-ylabel('Northing (m)')
-zlabel('Elevation (m)')
+xlabel('Relative Easting (m)')
+ylabel('Relative Northing (m)')
+zlabel('Relative Elevation (m)')
 grid on;
 title('3D Trajectory in UTM');
 
@@ -58,3 +63,32 @@ plot3(x(1),y(1),z(1), 'ro', 'Markersize', 10)
 plot3(x(1),y(1),z(1), 'rx', 'Markersize', 20)
 hold off
 legend('Trajectory', 'Initial pos')
+
+% Plot individually, to improve resolution
+if(nargin < 4)
+  f_handle = figure;
+else
+  figure(f_handle + 1)
+end
+subplot 131
+title('Individual relative measurements')
+hold on
+plot(x,sprintf('.-%c',path_color));
+xlabel('Time (s)')
+ylabel('Relative Easting (m)')
+hold off
+grid on
+subplot 132
+hold on
+plot(y,sprintf('.-%c',path_color));
+xlabel('Time (s)')
+ylabel('Relative Northing (m)')
+hold off
+grid on
+subplot 133
+hold on
+plot(z,sprintf('.-%c',path_color));
+xlabel('Time (s)')
+ylabel('Relative Elevation (m)')
+hold off
+grid on
