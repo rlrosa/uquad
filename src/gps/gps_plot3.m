c1ad7b0f540c62e78fbf7a3cf099f1ed0ec54305
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-01-21 12:55:00 -0200

    script para ayuda con la data de 10 min
    algunos cambios a los otros scripts del gps, para que colaboren con el de 10 min.
    
    modified:   gps_plot3.m
          imprimir sin el promedio
          imprimir sin mostrar la posicion initial
    modified:   gpxlogger_xml_handler.m
          se devuelve la data cruda (lat/lon) ad+ de la conversion a UTM
          se adapta a la nueva version de gps_plot3

--- src/gps/gps_plot3.m
+++ src/gps/gps_plot3.m
@@ -1,6 +1,8 @@
-function gps_plot3(easting, northing, elevation, sat, f_handle, fig_count)
+function gps_plot3(easting, northing, elevation, sat, ...
+  f_handle, fig_count, smooth_win)
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-% function gps_plot3(easting, northing, elevation, sat, f_handle, fig_count)
+% function gps_plot3(easting, northing, elevation, sat, ...
+% f_handle, fig_count, do_smooth)
 %
 % Generates 3d plot from GPS data.
 % 
@@ -11,7 +13,9 @@ function gps_plot3(easting, northing, elevation, sat, f_handle, fig_count)
 %   filename: Name of log file that provided the data.
 %   f_handle (optional): Existing plot handle, hold it and plot.
 %   fig_count (optional): Existing plot count, to use different colors per
-%   plot.
+%     plot.
+%   smooth_win (optional): Size of the window to use to calculate an
+%     average (moving average). If set to 0, no average will be performed.
 %
 % Example:
 %
@@ -21,12 +25,15 @@ function gps_plot3(easting, northing, elevation, sat, f_handle, fig_count)
 % gps_plot3(x_1, y_1, z_1, sat_1, figure(1), 1);
 % 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-path_colors = 'bgkyc';
+path_colors = 'bgkycr';
 x = easting;
 y = northing;
 z = elevation;
 
-if(nargin < 6)
+if(nargin < 7)
+  smooth_win = 0;
+end
+if((nargin < 6) || (fig_count == 0))
   fig_count = 0;
 else
   col_count = size(path_colors,2);
@@ -50,10 +57,11 @@ y = y - mean(y);
 z = z - mean(z);
 
 % calculate smoothed trayectory
-avg_size = 15;
-x_avg = moving_avg(x, avg_size);
-y_avg = moving_avg(y, avg_size);
-z_avg = moving_avg(z, avg_size);
+if(smooth_win)
+  x_avg = moving_avg(x, smooth_win);
+  y_avg = moving_avg(y, smooth_win);
+  z_avg = moving_avg(z, smooth_win);
+end
 
 path_color = path_colors(fig_count + 1);
 plot3(x, y, z,sprintf('.-%c',path_color));
@@ -65,61 +73,68 @@ title('3D Trajectory in UTM');
 
 % Display initial position.
 hold on;
-plot3(x(1),y(1),z(1), 'ro')
-plot3(x(1),y(1),z(1), 'ro', 'Markersize', 20)
-plot3(x(1),y(1),z(1), 'ro', 'Markersize', 10)
-plot3(x(1),y(1),z(1), 'rx', 'Markersize', 20)
+display_initial_pos = 1;
+if(display_initial_pos)
+  plot3(x(1),y(1),z(1), 'ro','HandleVisibility','off')
+  plot3(x(1),y(1),z(1), 'ro', 'Markersize', 20,'HandleVisibility','off')
+  plot3(x(1),y(1),z(1), 'ro', 'Markersize', 10,'HandleVisibility','off')
+  plot3(x(1),y(1),z(1), 'rx', 'Markersize', 20,'HandleVisibility','off')
+end
 
 % Plot smoothed trayectory
-plot3(x_avg, y_avg, z_avg,sprintf('x-%c',path_color),'LineWidth',2);
+if(smooth_win)
+  plot3(x_avg, y_avg, z_avg,sprintf('x-%c',path_color),'LineWidth',2);
+end
 
 hold off
-legend('Trajectory', 'Initial pos')
+legend('Trajectory')
 
 % Plot individually, to improve resolution
-if(nargin < 5)
-  f_handle = figure;
-else
-  figure(f_handle + 1)
-end
-subplot 131
-title('Individual relative measurements')
-hold on
-plot(x,sprintf('.-%c',path_color));
-plot(x_avg,sprintf('x-%c',path_color), 'LineWidth', 2);
-xlabel('Time (s)')
-ylabel('Relative Easting (m)')
-hold off
-grid on
-subplot 132
-hold on
-plot(y,sprintf('.-%c',path_color));
-plot(y_avg,sprintf('x-%c',path_color), 'LineWidth', 2);
-xlabel('Time (s)')
-ylabel('Relative Northing (m)')
-hold off
-grid on
-subplot 133
-hold on
-plot(z,sprintf('.-%c',path_color));
-plot(z_avg,sprintf('x-%c',path_color), 'LineWidth', 2);
-xlabel('Time (s)')
-ylabel('Relative Elevation (m)')
-hold off
-grid on
+plot_individual_data = 1;
+if plot_individual_data
+  if(nargin < 5)
+    f_handle = figure;
+  else
+    figure(f_handle + 1)
+  end
+  subplot 131
+  title('Individual relative measurements')
+  hold on
+  plot(x,sprintf('.-%c',path_color));
+  if(smooth_win)plot(x_avg,sprintf('x-%c',path_color), 'LineWidth', 2);end
+  xlabel('Time (s)')
+  ylabel('Relative Easting (m)')
+  hold off
+  grid on
+  subplot 132
+  hold on
+  plot(y,sprintf('.-%c',path_color));
+  if(smooth_win)plot(y_avg,sprintf('x-%c',path_color), 'LineWidth', 2);end
+  xlabel('Time (s)')
+  ylabel('Relative Northing (m)')
+  hold off
+  grid on
+  subplot 133
+  hold on
+  plot(z,sprintf('.-%c',path_color));
+  if(smooth_win)plot(z_avg,sprintf('x-%c',path_color), 'LineWidth', 2);end
+  xlabel('Time (s)')
+  ylabel('Relative Elevation (m)')
+  hold off
+  grid on
 
-% Plot individually, to improve resolution
-if(nargin < 5)
-  f_handle = figure;
-else
-  % inc one figure
-  figure(f_handle + 2)
+  % Plot individually, to improve resolution
+  if(nargin < 5)
+    f_handle = figure;
+  else
+    % inc one figure
+    figure(f_handle + 2)
+  end
+  hold on
+  plot(sat,sprintf('.-%c',path_color));
+  title('Satelites available')
+  xlabel('Time (s)')
+  ylabel('Satelite count')
+  hold off
+  grid on
 end
-hold on
-plot(sat,sprintf('.-%c',path_color));
-title('Satelites available')
-xlabel('Time (s)')
-ylabel('Satelite count')
-hold off
-grid on
-
