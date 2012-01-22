2edde71aeeebd6349677da21cf233437bb9be4e8
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-01-22 00:50:29 -0200

    scripts de ayuda. tmb un imprime circulos que encontre ahi

--- tests/gps/data/01_17_2012_logs/data10m.m
+++ tests/gps/data/01_17_2012_logs/data10m.m
@@ -11,16 +11,35 @@ end
 % Asignar un valor > 0 a la siguiente variable para generar un plot con
 % todos los datos (puede ser un quilombo)
 % Un valor distinto de cero imprime en una grilla
-todos_en_el_mismo_plot = 0;
+todos_en_el_mismo_plot = 1;
+
+radio = 2.5; % distancia en metros al valor medio
+
+easting_mean = zeros(6,1);
+northing_mean = zeros(6,1);
+elevation_mean = zeros(6,1);
+fuera_cantidad = zeros(6,1);
+fuera_porcentage = zeros(6,1);
 
 figure
 for i =1:6
-  subplot(320 + i)
   [easting, northing, elevation, utm_zone, sat] = ...
     gpxlogger_xml_handler(xs(i),0);
   if(todos_en_el_mismo_plot)
+    easting_mean(i) = mean(easting);
+    northing_mean(i) = mean(northing);
+    elevation_mean(i) = mean(elevation);
+    
+    fuera_cantidad(i) = sum(sqrt( ...
+      (easting - easting_mean(i)).^2 + ...
+      (northing - northing_mean(i)).^2 ...
+      ) ...
+      > radio);
+    fuera_porcentage(i) = fuera_cantidad(i)/length(easting);
+    
     gps_plot3(easting, northing, elevation, sat, gcf, i-1);
   else
+    subplot(320 + i)
     gps_plot3(easting, northing, elevation, sat, gcf, 0);
   end
 end
@@ -28,6 +47,9 @@ end
 if(todos_en_el_mismo_plot)
   legend('Pt. 1', 'Pt. 2', 'Pt. 3', 'Pt. 4', 'Pt. 5', 'Pt. 6')
   title('10 minutos en cada punto')
+  hold on
+  circle([0,0],radio,50,'-.k')
+  hold off
 end
 
 %% Arma una grilla con ejes en la misma escala
@@ -36,16 +58,18 @@ end
 % Fijar los siguiente valores mirando el plot existente. El objetivo es
 % hacer entrar todos lo puntos en un cierto rango (a determinar), y que
 % todos los plots tengan la misma escala, asi es + facil ver los errores.
-xmin = -3;
-xmax = 6;
-ymin = -5;
-ymax = 6;
-if(~exist('listo_logs','var'))
-  error('Fijar max/min de los ejes, luego definir una var listo_logs en el workspace (no importa el valor que se le asigne)')
-end
-for i =1:6
-  subplot(320 + i)
-  title(sprintf('Pt. #%d',i))
-  axis([xmin xmax ymin ymax])
-  legend hide
+if(~todos_en_el_mismo_plot)
+  xmin = -3;
+  xmax = 6;
+  ymin = -5;
+  ymax = 6;
+  if(~exist('listo_logs','var'))
+    error('Fijar max/min de los ejes, luego definir una var listo_logs en el workspace (no importa el valor que se le asigne)')
+  end
+  for i =1:6
+    subplot(320 + i)
+    title(sprintf('Pt. #%d',i))
+    axis([xmin xmax ymin ymax])
+    legend hide
+  end
 end
