38fe79d28730ae6989f168cc6680bfdd361946e0
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-01-31 18:32:28 -0200

    levantar todo lo que hay en scripts/ de forma recursiva

--- startup.m
+++ startup.m
@@ -6,13 +6,9 @@ if(isunix)
 else
   slash = '\';
 end
-% scripts
-addpath(sprintf('%s%cscripts',uquad_root,slash));
-% GPS support scripts
-addpath(sprintf('%s%cscripts%cgps',uquad_root,slash,slash));
 % IMU support scripts
-addpath(sprintf('%s%cscripts%cimu',uquad_root,slash,slash));
 addpath(genpath(sprintf('%s%ctests%cIMU',uquad_root,slash,slash)));
+addpath(genpath(sprintf('%s%cscripts',uquad_root,slash)));
 
 % set format to avoid confusing GPS data
 format long g
