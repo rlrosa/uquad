7501432bdfd7354a12d21cd4ce0259911924f68a
Matias Tailanian <matias@tailanian.com>
2012-02-18 14:50:22 -0200

    calibracion giroscopo terminada

--- tests/mongoose/cargo_nombres_gyro.m
+++ tests/mongoose/cargo_nombres_gyro.m
@@ -10,35 +10,35 @@ str_cont=str_cont+1;
 
 %% TALADRO
 
-for i=1:3
-    if i==1
-        eje='x';
-        otro='y';
-    elseif i==2
-        eje='y';
-        otro='z';
-    else
-        eje='z';
-        otro='x';
-    end
-    for j=1:2
-        if j==1
-           vel='v1';
-        elseif j==2
-            vel='v2';
-        end        
-        for k=2:2                   % OJO
-            if k==1
-                theta='00';
-            else
-                theta='30';
-            end
-            str=['gyro/logs/' eje vel otro theta];            
-            archivos{str_cont}=str;
-            str_cont=str_cont+1;
-        end
-    end
-end
+% for i=1:3
+%     if i==1
+%         eje='x';
+%         otro='y';
+%     elseif i==2
+%         eje='y';
+%         otro='z';
+%     else
+%         eje='z';
+%         otro='x';
+%     end
+%     for j=1:2
+%         if j==1
+%            vel='v1';
+%         elseif j==2
+%             vel='v2';
+%         end        
+%         for k=1:2
+%             if k==1
+%                 theta='00';
+%             else
+%                 theta='30';
+%             end
+%             str=['gyro/logs/' eje vel otro theta];            
+%             archivos{str_cont}=str;
+%             str_cont=str_cont+1;
+%         end
+%     end
+% end
 
 
 %% TOCADISCOS
@@ -56,7 +56,7 @@ for i=1:3
         eje='z';
         otro='y';
     end
-    for k=1:1                           % OJO
+    for k=1:3
         if k==1
             theta='00';
         elseif k==2
@@ -70,13 +70,5 @@ for i=1:3
     end
 
 end
-
-
-
-
-str=['gyro/logs/zv3y30'];       % OJO
-archivos{str_cont}=str;
-
-
 
 N=size(archivos,2);
