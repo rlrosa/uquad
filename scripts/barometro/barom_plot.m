b153270196d9c5de4e093bafa3fe41c403ee5fb0
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-02-22 19:49:31 -0200

    plot temp en otro eje y

--- scripts/barometro/barom_plot.m
+++ scripts/barometro/barom_plot.m
@@ -11,32 +11,67 @@ end
 
 figure;
 % plot data
-plot(alt(avg_size:end));
+section = [avg_size:length(alt)];
 hold on; 
-alt_moving_avg = moving_avg(alt,avg_size);
+alt_moving_avg = moving_avg(alt, avg_size);
 % plot avg
-plot(alt_moving_avg(avg_size:end),'g--');
-legend('Datos crudos',sprintf('Promedio en %d muestras',avg_size))
-
-if(exist('temp','var'))
-    plot(temp,'r')
-    legend('Datos crudos', ...
-      sprintf('Promedio. en %d muestras',avg_size), ...
-      'Perfil de temperatura')
-end
-
+hl1 = line(section, alt(section),'Color','b');
+hl2 = line(section,alt_moving_avg(section),'Color','g');
+legend('Datos crudos',sprintf('Promedio en %d muestras',avg_size), ...
+  'Location','NorthWest');
 ylabel('Altura respecto al nivel del mar (m)')
 xlabel('# de muestra')
+axis([1 length(alt) min(alt) max(alt)])
+
+if(exist('temp','var'))
+  % http://www.mathworks.com/help/techdoc/creating_plots/f1-11215.html
+  ax1 = gca;
+  set(ax1,'XColor','k','YColor','k')
+  ax2 = axes('Position',get(ax1,'Position'),...
+             'XAxisLocation','top',...
+             'YAxisLocation','right',...
+             'Color','none',...
+             'XColor','k','YColor','r');
+
+  % Draw the second set of data in the same color as the x- and y-axis.
+  hl3 = line(section,temp(section),'Color','r','Parent',ax2);
+
+  set(ax1,'XColor','k','YColor','k')
+  legend('Temperatura');
+  ylabel('Temperatura (Celsius)')
+
+%   %% Match grid
+%   % No hace falta, y queda horrible
+%   if(0)
+%     % Axis 1
+%     xlimits = get(ax1,'XLim');
+%     ylimits = get(ax1,'YLim');
+%     xinc = (xlimits(2)-xlimits(1))/5;
+%     yinc = (ylimits(2)-ylimits(1))/5;
+%     % Now set the tick mark locations.
+%     set(ax1,'XTick',[xlimits(1):xinc:xlimits(2)],...
+%             'YTick',[ylimits(1):yinc:ylimits(2)])
+%     % Axis 2
+%     xlimits = get(ax2,'XLim');
+%     ylimits = get(ax2,'YLim');
+%     xinc = (xlimits(2)-xlimits(1))/5;
+%     yinc = (ylimits(2)-ylimits(1))/5;
+%     % Now set the tick mark locations.
+%     set(ax2,'XTick',[xlimits(1):xinc:xlimits(2)],...
+%             'YTick',[ylimits(1):yinc:ylimits(2)])
+%   end
+end
+
 
 if(indexes ~= -1)
   % plot segment borders
-  max_val = max(alt);
   for i=1:length(indexes)
       line([indexes(i) indexes(i)],[min(alt) max(alt)],'color','black')
   end
-%stem(indexes)
 end
 
-axis([1 length(alt) min(alt) max(alt)])
+% rompe el multi eje
+%axis([1 length(alt) min(alt) max(alt)])
+
 grid on
 hold off
