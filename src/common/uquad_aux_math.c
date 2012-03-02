5c78ae1c3fdea1a8e3f4dd5961064c308fe71bb5
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-03-02 21:12:24 -0200

    init math cosas.

---
+++ src/common/uquad_aux_math.c
@@ -0,0 +1,18 @@
+void Matrix_Multiply(uquad_matrix *a, uquad_matrix *b, uquad_matrix *ans)
+{
+  float op[3]; 
+  for(int x=0; x<3; x++)
+  {
+    for(int y=0; y<3; y++)
+    {
+      for(int w=0; w<3; w++)
+      {
+       op[w]=a[x][w]*b[w][y];
+      } 
+      mat[x][y]=0;
+      mat[x][y]=op[0]+op[1]+op[2];
+      
+      //      float test=mat[x][y];
+    }
+  }
+}
