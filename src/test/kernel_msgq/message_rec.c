13f98f915db2b2e88bac6d1b429a577df8558351
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-03-08 18:58:56 -0200

    feature: varios tipos de mensages en kernel queues + ACK
    sirve para usar un tipo para setear velocidades, y otro para arrancar/frenar, y para no saturar de cosas obsoletas.

--- src/test/kernel_msgq/message_rec.c
+++ src/test/kernel_msgq/message_rec.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 #define MSGSZ     128
-
+#define LISTEN_T_US 500
 
 /*
  * Declare the message structure.
@@ -17,13 +17,14 @@ typedef struct msgbuf {
     char    mtext[MSGSZ];
 } message_buf;
 
-
 main()
 {
-    int msqid;
-    key_t key;
-    message_buf  rbuf;
-    int good;
+    int msqid, i;
+    key_t key, key_tx = 50;
+    int msgflg = IPC_CREAT | 0666;
+    message_buf  rbuf, sbuf;
+    char ack_buf[4] = "RXOK";
+    size_t buf_length = 4;
     struct timeval detail_time;
 
     /*
@@ -31,35 +32,68 @@ main()
      * "name" 1234, which was created by
      * the server.
      */
-    key = 1234;
+    key = 1;
     while(1)
     {
-	good = 1;
-	if ((msqid = msgget(key, 0666)) < 0) {
-	    good = 0;
+	key ^= 2;
+	// key_1
+	if ((msqid = msgget(key, 0666)) < 0)
+	{
+	    usleep(LISTEN_T_US);
+	    continue;
 	}
 
 	/*
 	 * Receive an answer of message type 1.
 	 */
-	if (good && msgrcv(msqid, &rbuf, 4, 1, 0) < 0) {
-	    good = 0;
+	if (msgrcv(msqid, &rbuf, buf_length, 1, IPC_NOWAIT) < 0)
+	{
+	    usleep(LISTEN_T_US);
+	    continue;
 	}
 
 	/*
 	 * Print the answer.
 	 */
-	if(good)
+	gettimeofday(&detail_time,NULL);
+	printf("%02X\t%02X\t%02X\t%02X\t%d\n",
+	       (int) (0xff & rbuf.mtext[0]),
+	       (int) (0xff & rbuf.mtext[1]),
+	       (int) (0xff & rbuf.mtext[2]),
+	       (int) (0xff & rbuf.mtext[3]),
+	       (int)detail_time.tv_usec); /* microseconds */
+	
+
+	/// send ACK
+	while(1)
 	{
-	    //	    printf("%s\n", rbuf.mtext);
+	    if ((msqid = msgget(key_tx, msgflg )) < 0)
+	    {
+		usleep(LISTEN_T_US);
+		continue;
+	    }
+
+	    /*
+	     * We'll send message type 1
+	     */ 
+	    sbuf.mtype = 1;
+	    for (i = 0; i < 4; ++i)
+		sbuf.mtext[i] = ack_buf[i];
+	    buf_length = 4;
+
+	    /*
+	     * Send a message.
+	     */
+	    if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0)
+	    {
+		usleep(LISTEN_T_US);
+		continue;
+	    }
 	    gettimeofday(&detail_time,NULL);
-	    printf("%02X\t%02X\t%02X\t%02X\t%d\n",
-		   0xff & rbuf.mtext[0],
-		   0xff & rbuf.mtext[1],
-		   0xff & rbuf.mtext[2],
-		   0xff & rbuf.mtext[3],
+	    printf("%d\n",
 		   (int)detail_time.tv_usec); /* microseconds */
-	}
+	    break;
+	} // ACK
     }
     exit(0);
 }
--- src/test/kernel_msgq/message_send.c
+++ src/test/kernel_msgq/message_send.c
@@ -9,6 +9,8 @@
 
 #define MSGSZ     128
 
+#define SEND_T_US 1000
+#define ACK_WAIT_SLEEP_US 100
 /*
  * Declare the message structure.
  */
@@ -21,48 +23,82 @@ main()
 {
     int msqid;
     int msgflg = IPC_CREAT | 0666;
-    key_t key;
-    struct timeval detail_time;
-    message_buf sbuf;
+    key_t key, key_rx = 50;
+    struct timeval time_tx, time_aux;
+    message_buf sbuf, rbuf;
     size_t buf_length;
-    int good = 1, i;
+    int i;
 
     /*
      * Get the message queue id for the
      * "name" 1234, which was created by
      * the server.
      */
-    key = 1234;
+    key = 1;
 
     while(1)
     {
-	good = 1;	
-	if ((msqid = msgget(key, msgflg )) < 0) {
-	    good = 0;
-	}
+	key ^= 2;
+	if ((msqid = msgget(key, msgflg )) < 0)
+	    continue;
 
 	/*
 	 * We'll send message type 1
 	 */ 
 	sbuf.mtype = 1;
     	uint8_t data[4] = {1,2,254,7};
+	char cbuf[4] = "teta";
 	for(i=0; i < 4; ++i)
-	    sbuf.mtext[i] = (char) data[i];
+	    if(key == 3)
+		sbuf.mtext[i] = (char) data[i];
+	    else
+		sbuf.mtext[i] = cbuf[i];
 	buf_length = 4;
 
 	/*
 	 * Send a message.
 	 */
-	if (good && msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
-	    good = 0;
+	if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0)
+	    continue;
+	gettimeofday(&time_tx,NULL);
+	printf("%d\n",
+	       (int)time_tx.tv_usec); /* microseconds */
+
+	usleep(SEND_T_US);
+
+	/// wait answer
+	while(1)
+	{		
+	    // key_rx
+	    if ((msqid = msgget(key_rx, 0666)) < 0)
+	    {
+		usleep(ACK_WAIT_SLEEP_US);
+		continue;
+	    }
+
+	    /*
+	     * Receive an answer of message type 1.
+	     */
+	    if (msgrcv(msqid, &rbuf, 4, 1, IPC_NOWAIT) < 0)
+	    {
+		usleep(ACK_WAIT_SLEEP_US);
+		continue;
+	    }
+
+	    /*
+	     * Print the answer.
+	     */
+	    gettimeofday(&time_aux,NULL);
+	    printf("ACK received!:\t%c%c%c%c\t%d\n",
+		   rbuf.mtext[0],
+		   rbuf.mtext[1],
+		   rbuf.mtext[2],
+		   rbuf.mtext[3],
+		   (int)time_aux.tv_usec); /* microseconds */
+	    /// if we got here then we received an ACK
+	    break;
 	}
-	else
-	{
-	    gettimeofday(&detail_time,NULL);
-	    printf("%d\n",
-		   (int)detail_time.tv_usec); /* microseconds */
-	}
-	sleep(1);
     }
+    /// never gets here
     exit(0);
 }
