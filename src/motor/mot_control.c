f50728aa0db2ca5b08918f00a9ffd4450707b58f
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-03-09 23:06:37 -0200

    motor control con kernel msg

--- src/motor/mot_control.c
+++ src/motor/mot_control.c
@@ -1,6 +1,10 @@
 #include "mot_control.h"
+#include <uquad_aux_time.h>
 #include <stdlib.h> // for malloc()
 #include <string.h> // for memset()
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 uquad_mot_t *mot_init(void)
 {
@@ -13,12 +17,6 @@ uquad_mot_t *mot_init(void)
     uquad_mot_t *m = (uquad_mot_t *)malloc(sizeof(uquad_mot_t));
     mem_alloc_check(m);
     memset((void *)m,0,sizeof(uquad_mot_t));
-    m->ctrl_file = fopen(MOT_COMM_FILE,"w");
-    if(m->ctrl_file == NULL)
-    {
-	err_log("Failed to open motor control file!");
-	retval = ERROR_FAIL;
-    }
     m->tx_log = fopen(MOT_LOG_TX,"w");
     if(m->tx_log == NULL)
     {
@@ -28,9 +26,10 @@ uquad_mot_t *mot_init(void)
     retval = system(START_MOTOR_CMD);
     if(retval < 0)
     {
-	err_log("Failed to run cmd_motores!");
-	retval = ERROR_FAIL;
+    	err_log("Failed to run cmd!");
+    	retval = ERROR_FAIL;
     }
+    sleep_ms(10);
     if(retval != ERROR_OK)
     {
 	mot_deinit(m);
@@ -39,30 +38,46 @@ uquad_mot_t *mot_init(void)
     return m;
 };
 
-static unsigned long tx_counter = 0;
 int mot_send(uquad_mot_t *mot)
 {
-    int retval, i;
-    retval = fprintf(mot->ctrl_file,"%d %d %d %d",
-		     mot->i2c_target[0],
-		     mot->i2c_target[1],
-		     mot->i2c_target[2],
-		     mot->i2c_target[3]);
-    fflush(mot->ctrl_file);
-    if(retval < MOT_C)
+    static unsigned long tx_counter = 0;
+    const static key_t key = 69;
+    static int msqid;
+    static int msgflg = IPC_CREAT | 0666;
+    static message_buf_t sbuf;
+    static size_t buf_length = (size_t)MOT_C;
+    static struct timeval tmp_tv, diff_tv;
+    int retval = ERROR_OK, i;
+
+    gettimeofday(&tmp_tv,NULL);
+    retval = uquad_timeval_substract(&diff_tv,tmp_tv,mot->last_set);
+    if(diff_tv.tv_usec < MOT_WAIT_US && diff_tv.tv_sec < 1)
     {
-	err_check(ERROR_MOTOR_SET,"Failed to write commands for all motors!");
+	err_check(ERROR_MOT_SATURATE,"Cannot change speed so often!");
+    }
+    if ((msqid = msgget(key, msgflg )) < 0)
+    {
+	err_check(ERROR_KQ,"msgget failed!");
+    }
+
+    /// We'll send message type 1
+    sbuf.mtype = 1;
+    for(i=0; i < MOT_C; ++i)
+	sbuf.mtext[i] = (char) mot->i2c_target[i];
+    /// Send a message.
+    if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0)
+    {
+	err_check(ERROR_KQ,"msgsnd failed!");
     }
     gettimeofday(&mot->last_set,NULL);
-
     /// Log sent data, timestamp, and counter
-    retval = fprintf(mot->tx_log,"%d %lu %d %d %d %d\n",
-		     (int)mot->last_set.tv_usec,
-		     tx_counter++,
+    retval = fprintf(mot->tx_log,"%d\t%d\t%d\t%d\t%d\t%lu\n",
 		     mot->i2c_target[0],
 		     mot->i2c_target[1],
 		     mot->i2c_target[2],
-		     mot->i2c_target[3]);
+		     mot->i2c_target[3],
+		     (int)mot->last_set.tv_usec,
+		     tx_counter++);
     memcpy(mot->i2c_curr,mot->i2c_target,MOT_C*sizeof(int));
     return ERROR_OK;
 }
@@ -70,29 +85,30 @@ int mot_send(uquad_mot_t *mot)
 int mot_rad2i2c(double w, int *i2c)
 {
     double w_2, w_3;
-    if(w < 0 || w > MAX_W)
+    if(w > MAX_W)
     {
 	err_check(ERROR_MOTOR_W,"Speed out of range!");
     }
-
     w_2 = w*w;
     w_3 = w_2*w;
     *i2c = uquad_round_double2int(0.0000022118*w_3 - 0.00071258*w_2 + 0.5106*w);
     return ERROR_OK;
 }
 
-int mot_set_vel_rads(uquad_mot_t *mot, int *w)
+int mot_set_vel_rads(uquad_mot_t *mot, double *w)
 {
     int i,retval;
     int itmp;
     
     for(i=0; i < MOT_C; ++i)
     {
-	retval = mot_rad2i2c(w[i],&itmp);
-	if(retval != ERROR_OK)
+	if(w[i] <= 0.0)
 	{
-	    mot->i2c_target[i] = itmp;
+	    err_check(ERROR_MOTOR_USAGE,"Use mot_stop() to stop motors!");
 	}
+	retval = mot_rad2i2c(w[i],&itmp);
+	err_propagate(retval);
+	mot->i2c_target[i] = itmp;
     }
     retval = mot_send(mot);
     err_propagate(retval);
@@ -101,10 +117,12 @@ int mot_set_vel_rads(uquad_mot_t *mot, int *w)
 
 int mot_set_idle(uquad_mot_t *mot)
 {
-    int retval = ERROR_OK;
-    memset(mot->i2c_target, MOT_I2C_IDLE, MOT_C*sizeof(int));
+    int retval = ERROR_OK,i ;
+    for (i=0; i < MOT_C; ++i)
+	mot->i2c_target[i] = MOT_I2C_IDLE;
     retval = mot_send(mot);
     err_propagate(retval);
+    sleep_ms(MOT_WAIT_STARTUP_MS);
     return retval;
 };
 
@@ -123,8 +141,6 @@ int mot_deinit(uquad_mot_t *mot)
     retval = system(KILL_MOTOR_CMD);
     if(mot != NULL)
     {
-	if(mot->ctrl_file != NULL)
-	    fclose(mot->ctrl_file);
 	if(mot->tx_log != NULL)
 	    fclose(mot->tx_log);
 	free(mot);
--- src/motor/mot_control.h
+++ src/motor/mot_control.h
@@ -2,6 +2,7 @@
 #define MOT_CONTROL_H
 
 #include <uquad_types.h>
+#include <uquad_aux_math.h>
 #include <uquad_aux_time.h>
 #include <uquad_error_codes.h>
 #include <stdio.h>
@@ -11,16 +12,19 @@
 #define MOT_I2C_IDLE 50
 #define MOT_C 4
 
-#define START_MOTOR_CMD "sh ~/.cmd"
-#define KILL_MOTOR_CMD "kill -INT `pidof -s cmd"
+#define MOT_WAIT_US 2000 // us
+#define MOT_WAIT_STARTUP_MS 420 // ms
+
+#define START_MOTOR_CMD "./cmd > /dev/null &"
+#define KILL_MOTOR_CMD "kill -INT `pidof -s cmd` &"
 
 /// Communication with i2c handling program si done through file
-#define MOT_COMM_FILE "mot_control.file"
+#define MOT_COMM_FILE "/tmp/mot_control.file"
 
 /// Log data
-#define MOT_LOG_TX "tx.log"
+#define MOT_LOG_TX "cmd_tx.log"
 
-#define MAX_W 422
+#define MAX_W 422.0
 
 /**
  * Structure to hold motor status.
@@ -33,12 +37,16 @@
  * 
  */
 typedef struct uquad_mot{
-    FILE *ctrl_file;
     FILE *tx_log;
     int i2c_curr[MOT_C];
     int i2c_target[MOT_C];
     struct timeval last_set;
 }uquad_mot_t;
+
+typedef struct msgbuf {
+    long    mtype;
+    char    mtext[MOT_C];
+} message_buf_t;
 
 /** 
  * Allocates memory for uquad_mot_t, opens log
@@ -58,7 +66,7 @@ uquad_mot_t *mot_init(void);
  * 
  * @return error code
  */
-int mot_set_vel_rads(uquad_mot_t *mot, int *w);
+int mot_set_vel_rads(uquad_mot_t *mot, double *w);
 
 /** 
  * Sets idle speed as target speed for all motors.
