64ad62fdd083e976a776085a7495d6c2c3ade659
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-03-11 19:35:57 -0300

    transpose

--- src/math/transpose_square_matrix.c
+++ src/math/transpose_square_matrix.c
@@ -1,36 +1,43 @@
 ////////////////////////////////////////////////////////////////////////////////
-// File: mul_matrix_by_scalar.c                                               //
+// File: transpose_square_matrix.c                                            //
 // Routine(s):                                                                //
-//    Multiply_Matrix_by_Scalar                                               //
+//    Transpose_Square_Matrix                                                 //
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-//  void Multiply_Matrix_by_Scalar(double *A, double x, int nrows, int ncols) //
+//  void Transpose_Square_Matrix( double *A, int n )                          //
 //                                                                            //
 //  Description:                                                              //
-//     Multiply each element of the matrix A by the scalar x.                 //
+//     Take the transpose of A and store in place.                            //
 //                                                                            //
 //  Arguments:                                                                //
 //     double *A    Pointer to the first element of the matrix A.             //
-//     double x     Scalar to multipy each element of the matrix A.           //
-//     int    nrows The number of rows of matrix A.                           //
-//     int    ncols The number of columns of the matrix A.                    //
+//     int    n     The number of rows and columns of the matrix A.           //
 //                                                                            //
 //  Return Values:                                                            //
 //     void                                                                   //
 //                                                                            //
 //  Example:                                                                  //
 //     #define N                                                              //
-//     #define M                                                              //
-//     double A[M][N],  x;                                                    //
+//     double A[N][N];                                                        //
 //                                                                            //
-//     (your code to initialize the matrix A and scalar x)                    //
+//     (your code to initialize the matrix A)                                 //
 //                                                                            //
-//     Multiply_Matrix_by_Scalar(&A[0][0], x, M, N);                          //
-//     printf("The matrix A is \n"); ...                                      //
+//     Transpose_Square_Matrix( &A[0][0], N);                                 //
+//     printf("The transpose of A is \n"); ...                                //
 ////////////////////////////////////////////////////////////////////////////////
-void Multiply_Matrix_by_Scalar(double *A, double x, int nrows, int ncols) 
+void Transpose_Square_Matrix( double *A, int n ) 
 {
-   register int n = nrows * ncols - 1;
-   
-   for (; n >= 0; n--) A[n] *= x;
+   double *pA, *pAt;
+   double temp;
+   int i,j;
+
+   for (i = 0; i < n; A += n + 1, i++) {
+      pA = A + 1;
+      pAt = A + n;
+      for (j = i+1 ; j < n; pA++, pAt += n, j++) {
+         temp = *pAt;
+         *pAt = *pA;
+         *pA = temp;
+      } 
+   }
 }
