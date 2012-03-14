317a55844c3f8c746f885ed8f228f03acdc77275
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-03-14 15:18:03 -0300

    lee logs raw imu

--- tests/mongoose/m/mong_read.m
+++ tests/mongoose/m/mong_read.m
@@ -1,9 +1,13 @@
-function [a,w,c,t,b,fecha,ind,T] = mong_read(file,plotear)
+function [a,w,c,t,b,fecha,ind,T] = mong_read(file,plotear,imu_raw)
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 % function [a,w,c,t,b,fecha,ind,T] = mong_read(file,plotear)
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if(nargin < 2)
   plotear = 1;
+end
+
+if(~exist('imu_raw','var'))
+  imu_raw = 0;
 end
 
 F    = fopen(file);
@@ -38,19 +42,25 @@ c    = zeros(Nn,3); % Datos del compas
 t    = zeros(Nn,1); % Datos del termómetro
 b    = zeros(Nn,1); % Datos del barómetro
 
-T(:,1)=str2double(D{1}(2:cols:end));
+if(imu_raw)
+  i = 1;
+else
+  i = 0;
+end
+
+T(:,1)=str2double(D{1}(i+2:cols:end));
 T = T * 1e-6; % Paso de microsegundos a segundos
-a(:,1)=str2double(D{1}(3:cols:end));
-a(:,2)=str2double(D{1}(4:cols:end));
-a(:,3)=str2double(D{1}(5:cols:end));
-w(:,1)=str2double(D{1}(6:cols:end));
-w(:,2)=str2double(D{1}(7:cols:end));
-w(:,3)=str2double(D{1}(8:cols:end));
-c(:,1)=str2double(D{1}(9:cols:end));
-c(:,2)=str2double(D{1}(10:cols:end));
-c(:,3)=str2double(D{1}(11:cols:end));
-t(:,1)=str2double(D{1}(12:cols:end));
-b(:,1)=str2double(D{1}(13:cols:end));
+a(:,1)=str2double(D{1}(i+3:cols:end));
+a(:,2)=str2double(D{1}(i+4:cols:end));
+a(:,3)=str2double(D{1}(i+5:cols:end));
+w(:,1)=str2double(D{1}(i+6:cols:end));
+w(:,2)=str2double(D{1}(i+7:cols:end));
+w(:,3)=str2double(D{1}(i+8:cols:end));
+c(:,1)=str2double(D{1}(i+9:cols:end));
+c(:,2)=str2double(D{1}(i+10:cols:end));
+c(:,3)=str2double(D{1}(i+11:cols:end));
+t(:,1)=str2double(D{1}(i+12:cols:end));
+b(:,1)=str2double(D{1}(i+13:cols:end));
 
 if(plotear)
   figure()
