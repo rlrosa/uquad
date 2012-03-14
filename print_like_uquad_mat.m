f875a5f3a4298e1f2c2a6ca382dbb715640c97fa
Matias Tailanian <matias@tailanian.com>
2012-03-14 20:36:53 -0300

    script que imprime de forma q C entiende y kalman con control integrado

---
+++ print_like_uquad_mat.m
@@ -0,0 +1,8 @@
+function [] = print_like_uquad_mat(struct,nombre_matriz,A)
+[M,N] = size(A);
+
+for i=1:M
+    for j=1:N
+        fprintf('%s -> %s -> m[%d][%d] = %0.10f;\n',struct,nombre_matriz,i-1,j-1,A(i,j));
+    end
+end
