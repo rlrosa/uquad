7b2e07f52103a03cd65d9a0f0de44b6ab81e8e8f
Matias Tailanian <matias@tailanian.com>
2012-03-15 19:37:10 -0300

    ajuste por temperatura de ganancia y offset sin exito

--- startup.m
+++ startup.m
@@ -11,6 +11,7 @@ addpath(genpath(sprintf('%s%ctests',uquad_root,slash)));
 addpath(sprintf('%s%csniffer',uquad_root,slash));
 addpath(genpath(sprintf('%s%cscripts',uquad_root,slash)));
 addpath(genpath(sprintf('%s%ckalman',uquad_root,slash)));
+addpath(genpath(sprintf('%s%csimulador',uquad_root,slash)));
 
 % set format to avoid confusing GPS data
 format long g
