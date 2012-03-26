6c4a9f793592ea05c63863cd6bdde5a11f462814
spaternain <spaternain@gmail.com>
2012-03-26 13:04:38 -0300

    Un cambio en la función print_like_uquad para tener más dígitos después de la coma

--- print_like_uquad_mat.m
+++ print_like_uquad_mat.m
@@ -3,6 +3,6 @@ function [] = print_like_uquad_mat(struct,nombre_matriz,A)
 
 for i=1:M
     for j=1:N
-        fprintf('%s -> %s -> m[%d][%d] = %0.10f;\n',struct,nombre_matriz,i-1,j-1,A(i,j));
+        fprintf('%s -> %s -> m[%d][%d] = %0.20f;\n',struct,nombre_matriz,i-1,j-1,A(i,j));
     end
 end
