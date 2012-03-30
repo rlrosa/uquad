3d51480743e83190724888e871fa7b912c237403
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-03-30 15:19:01 -0300

    scripts para simplificar debugging de los logs de c

--- tests/mongoose/m/plot_c.m
+++ tests/mongoose/m/plot_c.m
@@ -4,12 +4,7 @@ kin     = load([path 'kalman_in.log']);
 x_hat_c = load([path 'x_hat.log'    ]);
 wlog    = load([path 'w.log'        ]);
 
-a     = kin(:,5:7);
-w     = kin(:,8:10);
-euler = kin(:,11:13);
-% t     = kin(:,14);
-b     = kin(:,end);
-z = [euler a w b];
+z = kin2z(kin);
 
 plot_main(x_hat_c,z)
 plot_w(wlog(:,2:end))
