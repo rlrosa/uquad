20237dc0edf780fa0ae0585c8ea5b866a7e39a1b
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-03 10:10:20 -0300

    fix: usar host en lugar de ping, funca mejor.

--- scripts/check_net.sh
+++ scripts/check_net.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# Check if comm with server is running, if not, kill motors.
+# Check if comm with server is running, if not, kill cmd.
+
+# wait for cmd startup
+sleep 4
+
+# check connections
 while(true);
 do
-    x=`ping -c1 10.42.43.1 2>&1 | grep unknown`
+    x=`host 10.42.43.1 2>&1 | grep NXDOMAIN`
     if [ ! "$x" = "" ]; then
+	echo "Connection lost!"
 	kill -9 `pidof -s cmd`
-    else
-	echo ok!
+	exit
     fi
     sleep 1
 done
