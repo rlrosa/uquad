33b9a0af6d4852c44ca25531bd78461f79ca1631
spaternain <spaternain@gmail.com>
2012-04-11 10:07:32 -0300

    Con integradores

--- simulador/pruebas/primer_vuelo/simula_primer_vuelo.m
+++ simulador/pruebas/primer_vuelo/simula_primer_vuelo.m
@@ -5,45 +5,32 @@ A=Matrices.A;
 B=Matrices.B;
 
 
-%Saco todas las variables que no vamos a usar;
-A(7:8,:)=[];
-A(1:2,:)=[];
-A(:,7:8)=[];
-A(:,1:2)=[];
-B(7:8,:)=[];
-B(1:2,:)=[];
-
 wq1=0; wq2=0; wq3=0;
 
 vq1=0; vq2=0; vq3=0;
 
-psis=0; phi=0;  
+psis=0; phi=0; theta =-90;  
+
 
 w1=316.103650939028;
 w2=316.103650939028;
 w3=316.103650939028;
 w4=316.103650939028;
 
-% w1=290;
-% w2=290;
-% w3=290;
-% w4=290;
-
 A=eval(A);
 B=eval(B);
 
 
-%AZ = zeros(8,4);  
+
 C = eye(8,8);
 Z = zeros(8,8);
 BZ = zeros(8,4);
 
-%Q=diag([1 1e2 1e2 1e2 1 1 1 1 1 1 1 1 1 1 1 1]);%Pesos de z,psi,phi,theta, vqz wqx,wqy,wqz
-Q=diag([1 1e2 1e2 1e2 1 1 1 1]);
-R=1e-2*diag([1 1 1 1]); %Pesos de w1 w2 w3 w4;
+Q=diag([1 1 1 1e2 1e2 1e2 1 1 1 1 1 1 ]);
+R=1e-2*diag([1 1 1 1]); 
 
 %[K,S,E]=lqrd([A Z; C Z],[B;BZ],Q,R,10e-3);
-[K,S,E]=lqrd([A],[B],Q,R,10e-3);
+K=uquad_dlqr(A,B,Q,R);
 save('K','K');
 assignin('base','K',K);
 
