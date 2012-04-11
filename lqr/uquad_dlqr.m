3133d67f25bf1421bbc8f692f537210e73790fde
spaternain <spaternain@gmail.com>
2012-04-11 10:06:59 -0300

    Funciones que calculan la matriz de realimentación

--- lqr/uquad_dlqr.m
+++ lqr/uquad_dlqr.m
@@ -2,12 +2,12 @@ function K=uquad_dlqr(A,B,Q,R)
 
 rows=length(B(:,1));
 cols=length(B(1,:));
-Ts=evalin('base','Ts');
-%Ts = 10e-3;
-%syms s;
+%Ts=evalin('base','Ts');
+Ts = 10e-3;
+
 
 %Pique de wikipedia http://en.wikipedia.org/wiki/Discretization
-D = uquad_exp([A B;zeros(4,28)]*Ts);
+D = uquad_exp([A B;zeros(cols,rows+cols)]*Ts);
 A = D(1:rows,1:rows);
 B = D(1:rows,rows+1:end);
 
--- src/path_planner/path_planner.c
+++ src/path_planner/path_planner.c
@@ -81,51 +81,74 @@ void pp_deinit(path_planner_t *pp)
 /* Where phi=exp(A*Ts) and gama=int(exp(A*s),0,Ts) */
 int pp_update_K(path_planner_t *pp)
 {
+    int retval;
     uquad_mat_t *phi = NULL;
     uquad_mat_t *gamma = NULL;
     
     //Discretization of the system;
     phi = uquad_mat_alloc(pp->A->r,pp->A->c);
     gamma = uquad_mat_alloc(pp->B->r,pp->B->c);
-    pp_disc(phi, gamma, pp->A, pp->B, pp->Ts);
-    
+    retval = pp_disc(phi, gamma, pp->A, pp->B, pp->Ts);
+    cleanup_if(retval);
     //Obtains feedback matrix  
-    pp_lqr(pp->K, phi, gamma, pp->Q, pp->R);
-    return ERROR_OK;
+    retval = pp_lqr(pp->K, phi, gamma, pp->Q, pp->R);
+    cleanup_if(retval);
+
+    cleanup:
+    uquad_mat_free(phi);
+    uquad_mat_free(gamma);
+
+    return retval;
 }
 /*Returns the discrete form of a system \dot{x} = AX+BU*/
 int pp_disc(uquad_mat_t *phi,uquad_mat_t *gamma,uquad_mat_t *A,uquad_mat_t *B, double Ts)
 {
+    int retval;
     uquad_mat_t *aux0 = NULL;
     uquad_mat_t *aux1 = NULL;
     
-    phi = uquad_mat_alloc(A->r,A->c);
-    gamma = uquad_mat_alloc(B->r,B->c);
     aux0 = uquad_mat_alloc(A->r+B->c,A->c+B->c);
     aux1 = uquad_mat_alloc(A->r+B->c,A->c+B->c);
 
-    uquad_mat_set_subm(aux0, 0,0,A);
-    uquad_mat_set_subm(aux0, 0,A->c,B);
-    uquad_mat_scalar_mul(aux0,aux0,Ts); 
-    uquad_mat_exp(aux1,aux0);
-    
-    uquad_mat_get_subm(phi,0,0,aux1);
-    uquad_mat_get_subm(gamma,0,A->c,aux1);
-
+    retval = uquad_mat_set_subm(aux0, 0,0,A);
+    cleanup_if(retval);
+    retval =  uquad_mat_set_subm(aux0, 0,A->c,B);
+    cleanup_if(retval);
+    retval = uquad_mat_scalar_mul(aux0,aux0,Ts); 
+    cleanup_if(retval);
+    retval = uquad_mat_exp(aux1,aux0);
+    cleanup_if(retval);
+    retval= uquad_mat_get_subm(phi,0,0,aux1);
+    cleanup_if(retval);
+    retval = uquad_mat_get_subm(gamma,0,A->c,aux1);
+    cleanup_if(retval);
     //Cleaning
+    cleanup:
     uquad_mat_free(aux0);
     uquad_mat_free(aux1);
+
+    return retval;
 
 }
  //Solving riccati equation. Algorithm by Anders Friis Sorensen    
 int pp_lqr(uquad_mat_t *K, uquad_mat_t *A, uquad_mat_t *B, uquad_mat_t *Q, uquad_mat_t *R)
 {
-    int norm;
+    int retval;
+    double norm;
     uquad_mat_t *aux0 = NULL;
     uquad_mat_t *aux1 = NULL;
     uquad_mat_t *aux2 = NULL;
     uquad_mat_t *aux3 = NULL;
     uquad_mat_t *aux4 = NULL;
+    uquad_mat_t *aux5 = NULL;
+    uquad_mat_t *aux6 = NULL;
+    uquad_mat_t *aux7 = NULL;
+    uquad_mat_t *aux8 = NULL;
+    uquad_mat_t *aux9 = NULL;
+    uquad_mat_t *aux10 = NULL;
+    uquad_mat_t *aux11 = NULL;
+    uquad_mat_t *aux12 = NULL;
+    uquad_mat_t *aux13 = NULL;
     uquad_mat_t *P = NULL;
     uquad_mat_t *k = NULL;
     uquad_mat_t *Bt = NULL;
@@ -133,80 +156,117 @@ int pp_lqr(uquad_mat_t *K, uquad_mat_t *A, uquad_mat_t *B, uquad_mat_t *Q, uquad
     P = uquad_mat_alloc(Q->r,Q->r);
     k = uquad_mat_alloc(K->r,K->c);
 
-    uquad_mat_copy(P,Q);
-    uquad_mat_copy(k,K);
-
+    retval = uquad_mat_copy(P,Q);
+    cleanup_if(retval);
+    retval = uquad_mat_copy(k,K);
+    cleanup_if(retval);
     Bt = uquad_mat_alloc(B->c,B->r);
    
-    uquad_mat_transpose(Bt,B);
+    retval = uquad_mat_transpose(Bt,B);
+    cleanup_if(retval);
     norm=1;
+
+
+    aux0 = uquad_mat_alloc(B->r,B->c);
+    aux1 = uquad_mat_alloc(R->r,R->c);
+    aux2 = uquad_mat_alloc(R->r,R->c);
+    aux3 = uquad_mat_alloc(R->r,R->c);
+    aux4 = uquad_mat_alloc(2*R->r,2*R->c);
+    aux5 = uquad_mat_alloc(R->r,Q->c);
+    aux6 = uquad_mat_alloc(R->r,Q->c);
+    aux7 = uquad_mat_alloc(K->c,K->r);
+    aux8 = uquad_mat_alloc(K->c,K->r);
+    aux9 = uquad_mat_alloc(Q->r,Q->c);
+    aux10 = uquad_mat_alloc(Q->r,Q->c);	
+    aux11 = uquad_mat_alloc(Q->c,Q->r);
+    aux12 = uquad_mat_alloc(Q->c,Q->r);
+    aux13 = uquad_mat_alloc(K->r,K->c);
+    
     while (norm>1e-5)
     {
-        aux0 = uquad_mat_alloc(B->r,B->c);
-	aux1 = uquad_mat_alloc(R->r,R->c);
-        aux2 = uquad_mat_alloc(R->r,R->c);
-	aux3 = uquad_mat_alloc(R->r,R->c);
-	aux4 = uquad_mat_alloc(2*R->r,2*R->c);
+      
 
-	uquad_mat_copy(k,K);
 
+	retval = uquad_mat_copy(k,K);
+	cleanup_if(retval);
 	//Performs K=-(R+B'*P*B)^(-1)*B'*P*A; 
-	uquad_mat_prod(aux0,P,B);
-	uquad_mat_prod(aux1,Bt,aux0);
-	uquad_mat_add(aux1,aux1,R);
-	uquad_mat_inv(aux2,aux1,aux3,aux4);
+	retval = uquad_mat_prod(aux0,P,B);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux1,Bt,aux0);
+	cleanup_if(retval);
+	retval = uquad_mat_add(aux1,aux1,R);
+	cleanup_if(retval);
+	#warning "uquad_mat_inv is allocating"
+	retval = uquad_mat_inv(aux2,aux1,aux3,aux4);  
+	cleanup_if(retval);
 	
-	uquad_mat_free(aux0);
-	uquad_mat_free(aux1);
-	aux0 = uquad_mat_alloc(R->r,Q->c);
-	aux1 = uquad_mat_alloc(R->r,Q->c);
-	uquad_mat_prod(aux0,aux2,Bt);
-	uquad_mat_prod(aux1,aux0,P);
-	uquad_mat_prod(aux0,aux1,A);
-	uquad_mat_scalar_mul(K,aux0,-1);
+	retval = uquad_mat_prod(aux5,aux2,Bt);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux6,aux5,P);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux5,aux6,A);
+	cleanup_if(retval);
+	retval = uquad_mat_scalar_mul(K,aux5,-1);
+	cleanup_if(retval);
 	
-	uquad_mat_free(aux0);
-	uquad_mat_free(aux1);
-	uquad_mat_free(aux2);
-	uquad_mat_free(aux3);
-	uquad_mat_free(aux4);
-
+	
 	//Performs P=Q+K'*R*K+(A+B*K)'*P*(A+B*K);
-	aux0 = uquad_mat_alloc(K->c,K->r);
-	aux1 = uquad_mat_alloc(K->c,K->r);
-	aux2 = uquad_mat_alloc(Q->r,Q->c);
-	aux3 = uquad_mat_alloc(Q->r,Q->c);
-
-	//K'*R*K'
-	uquad_mat_transpose(aux0,K);
-	uquad_mat_prod(aux1,aux0,R);
-	uquad_mat_prod(aux2,aux1,K);
 	
-	uquad_mat_free(aux0);
-	uquad_mat_free(aux1);
+	//K'*R*K
+	retval = uquad_mat_transpose(aux7,K);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux8,aux7,R);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux9,aux8,K);
+	cleanup_if(retval);
+	
 
 	//A+B*K
-	uquad_mat_prod(aux3,B,K);
-	uquad_mat_add(aux3,aux3,A);
+	retval = uquad_mat_prod(aux10,B,K);
+	cleanup_if(retval);
+	retval = uquad_mat_add(aux10,aux10,A);
+	cleanup_if(retval);
 	
-	aux0 = uquad_mat_alloc(Q->c,Q->r);
-	aux1 = uquad_mat_alloc(K->c,K->r);
 
-	uquad_mat_transpose(aux0,aux3);
-	uquad_mat_prod(aux1,aux0,P);
-	uquad_mat_prod(aux0,aux1,aux3);
+	retval = uquad_mat_transpose(aux11,aux10);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux12,aux11,P);
+	cleanup_if(retval);
+	retval = uquad_mat_prod(aux11,aux12,aux10);
+	cleanup_if(retval);
 	
-	uquad_mat_free(aux1);
-	uquad_mat_free(aux3);
+	retval = uquad_mat_add(P,Q,aux9);
+	cleanup_if(retval);
+	retval = uquad_mat_add(P,P,aux11);
+	cleanup_if(retval);
 
-	uquad_mat_add(P,P,Q);
-	uquad_mat_add(P,P,aux2);
-	uquad_mat_add(P,P,aux0);
-
-	uquad_mat_free(aux0);
-	uquad_mat_free(aux1);
+	retval = uquad_mat_sub(aux13,K,k);
+	cleanup_if(retval);
+	
+	norm = uquad_mat_norm(aux13);
     }
 
-    uquad_mat_scalar_mul(K,K,-1);
+    retval = uquad_mat_scalar_mul(K,K,-1);
+    cleanup_if(retval);
+    
+
+    cleanup:
+    uquad_mat_free(aux0);
+    uquad_mat_free(aux1);
+    uquad_mat_free(aux2);
+    uquad_mat_free(aux3);
+    uquad_mat_free(aux4);
+    uquad_mat_free(aux5);
+    uquad_mat_free(aux6);
+    uquad_mat_free(aux7);
+    uquad_mat_free(aux8);
+    uquad_mat_free(aux9);
+    uquad_mat_free(aux10);
+    uquad_mat_free(aux11);
+    uquad_mat_free(aux12);
+    uquad_mat_free(k);
+    uquad_mat_free(P);
+    
+    return retval;
 }
 
