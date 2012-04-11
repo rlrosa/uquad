aa66f90c9869fe9cc48e1a92fd6b90ad0fe077c3
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-11 13:17:48 -0300

    pruebas de tiempos para debug en la beagle

---
+++ src/test/timing/timing.c
@@ -0,0 +1,76 @@
+#include <uquad_aux_time.h>
+#include <uquad_error_codes.h>
+#include <uquad_logger.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/signal.h>   // for SIGINT and SIGQUIT
+#include <unistd.h>       // for STDIN_FILENO
+
+#define TIME_LOG_NAME "time"
+#define LOOP_SLEEP_US 0
+
+FILE * log_time;
+
+void quit()
+{
+    uquad_logger_remove(log_time);
+    exit(0);
+}
+
+void uquad_sig_handler(int signal_num){
+    err_log_num("Caught signal:",signal_num);
+    quit();
+}
+
+
+int main(void){
+    int retval;
+    int counter = 0;
+    struct timeval tv_diff,t1,t2;
+    
+
+    log_time = uquad_logger_add(TIME_LOG_NAME);
+    if(log_time == NULL)
+    {
+	quit_log_if(ERROR_IO, "Failed to add time log!");
+    }
+
+    retval = gettimeofday(&t2,NULL);
+    if(retval != ERROR_OK)
+    {
+	err_log_std("Failed to get t2!");
+	quit_if(ERROR_FAIL);
+    }
+
+    for(;;)
+    {
+	retval = gettimeofday(&t1,NULL);
+	if(retval != ERROR_OK)
+	{
+	    err_log_std("Failed to get t1!");
+	    quit_if(ERROR_FAIL);
+	}
+	retval = uquad_timeval_substract(&tv_diff, t1, t2);
+	if(retval < 0)
+	{
+	    quit_log_if(ERROR_TIMING,"Absurd timing!");
+	}
+	log_int_only(log_time, counter++);
+	log_tv_only(log_time, tv_diff);
+	log_eol(log_time);
+
+	retval = gettimeofday(&t2,NULL);
+	if(retval != ERROR_OK)
+	{
+	    err_log_std("Failed to get t2!");
+	    quit_if(ERROR_FAIL);
+	}
+#if LOOP_SLEEP_US
+	usleep(LOOP_SLEEP_US);
+#endif
+    }
+
+    // never gets here
+    return ERROR_OK;
+}
