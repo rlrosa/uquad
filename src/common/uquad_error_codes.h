ce882c69bbe38aa5e04f22d862e77e0966f2951e
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-13 17:57:19 -0300

    + macros - log un tab

--- src/common/uquad_error_codes.h
+++ src/common/uquad_error_codes.h
@@ -88,6 +88,7 @@ ERROR_TIMING
 #define log_double(log,msg,dbl) fprintf(log,"%s:%s:%d: %s: %lf\n",__TIME__,__FILE__,__LINE__,msg,dbl)
 
 #define log_eol(log) fprintf(log,"\n")
+#define log_tab(log) fprintf(log,"\t")
 
 /**
  * Print timeval message with unsigned long to stderr
