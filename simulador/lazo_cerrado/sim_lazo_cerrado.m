23adaa2bc531e88ae073c6e1c49ea869d54e31a7
Matias Tailanian <matias@tailanian.com>
2012-04-14 21:14:06 -0300

    embellezco el ploteo del simulador

--- simulador/lazo_cerrado/sim_lazo_cerrado.m
+++ simulador/lazo_cerrado/sim_lazo_cerrado.m
@@ -134,10 +134,6 @@ modelo=['lazo_cerrado_' modo '_dis'];
 
 
 %% Grafico la trayectoria
-plot3(X(:,1),X(:,2),X(:,3));
-grid
-hold on;
-
 
 syms a b c
 Id=diag([1 1 1]);
@@ -152,14 +148,31 @@ R_psis=[1 0 0; 0 cos(c) sin(c) ;0 -sin(c) cos(c)];
 m_T_q =simple((Id/(R_theta))*(Id/(R_phi))*(Id/(R_psis)));
 
 
-i=1;
+green = [154,205,50]/256;
+blue  = [0,0,128]/256;
+red   = [184,69,57]/256;
 
+plot3(X(:,1),X(:,2),X(:,3));
+
+i=1;
+c=X(20*(i-1)+1,4); % yaw
+b=X(20*(i-1)+1,5); % pitch
+a=X(20*(i-1)+1,6); % roll
+R=eval(m_T_q);
+D=[X(20*(i-1)+1,1)*ones(1,6);X(20*(i-1)+1,2)*ones(1,6);X(20*(i-1)+1,3)*ones(1,6)];
+T=(R*[1 0 0 0 0 0 ; 0 0 1 0 0 0 ;0 0 0 0 1 0]);
+D=D+T;
+i=i+1;
+plot3(D(1,1:2),D(2,1:2),D(3,1:2),'color',blue);
+hold on; grid
+plot3(D(1,3:4),D(2,3:4),D(3,3:4),'color',red);
+plot3(D(1,5:6),D(2,5:6),D(3,5:6),'color',green);
+legend('x_q','y_q','z_q');
 while i<=length(X(:,1))/20
     
     c=X(20*(i-1)+1,4); % yaw
     b=X(20*(i-1)+1,5); % pitch
-    a=X(20*(i-1)+1,6); % roll
-    
+    a=X(20*(i-1)+1,6); % roll    
     
     R=eval(m_T_q);
     
@@ -168,11 +181,13 @@ while i<=length(X(:,1))/20
     D=D+T;
     i=i+1;
     
-    plot3(D(1,:),D(2,:),D(3,:));
-    grid
+    plot3(D(1,1:2),D(2,1:2),D(3,1:2),'color',blue);
+    plot3(D(1,3:4),D(2,3:4),D(3,3:4),'color',red);
+    plot3(D(1,5:6),D(2,5:6),D(3,5:6),'color',green);
+
     %pause(20*1e-2)
-    %hold on
-    
-  end
+    %hold on    
+end
+plot3(X(:,1),X(:,2),X(:,3),'color','black','linewidth',3);
+% axis equal
 hold off
-%axis equal
