83fdfd640817980c48e9d70f5ba0df92626d01bb
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-18 13:44:04 -0300

    define para los tiempos de espera al gps en el arranque del main

--- src/common/uquad_config.h
+++ src/common/uquad_config.h
@@ -4,9 +4,11 @@
 #define DEBUG             1 // Show debug info
 
 #define GPS_FAKE          1 // Simulate GPS data (use zeros)
+#define GPS_INIT_TOUT_S   60// GPS init timeout in sec
+#define GPS_INIT_TOUT_US  0 // GPS init timeout in sec
 
 #define CTRL_INTEGRAL     0 // Use PI control
-#define FULL_CONTROL      1 // Control 12 states
+#define FULL_CONTROL      0 // Control 12 states
 
 #if FULL_CONTROL
 #define STATES_CONTROLLED 12
