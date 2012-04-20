f8eab691dba9ebe9f2af0e580d8a104a292e07b3
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-20 12:27:36 -0300

    gps on/off en src/CMakelists.txt, asi puedo usar el define en todos lados
    antes solamente se definia o no en el main.

--- src/common/uquad_config.h
+++ src/common/uquad_config.h
@@ -8,6 +8,11 @@
 #define GPS_FAKE          1 // Simulate GPS data (use zeros)
 #define GPS_INIT_TOUT_S   60// GPS init timeout in sec
 #define GPS_INIT_TOUT_US  0 // GPS init timeout in sec
+#if (!USE_GPS && GPS_FAKE)
+#error							\
+    "Cannot use fake GPS without USE_GPS enabled!"	\
+    "Edit src/CMakelists.txt to enable USE_GPS"
+#endif // (!USE_GPS && GPS_FAKE)
 
 #define CTRL_INTEGRAL     0 // Use PI control
 #define FULL_CONTROL      0 // Control 12 states
