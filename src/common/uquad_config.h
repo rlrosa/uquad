34cc63bf6c91f7aae96aefac0e7388c5f3641a53
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-21 11:21:47 -0300

    reorganizo para que config solo tenga config

--- src/common/uquad_config.h
+++ src/common/uquad_config.h
@@ -6,18 +6,11 @@
 #define DEBUG             1 // Show debug info
 
 #define GPS_FAKE          0 // Simulate GPS data (use zeros)
-#define GPS_INIT_TOUT_S   60// GPS init timeout in sec
-#define GPS_INIT_TOUT_US  0 // GPS init timeout in sec
-#if (!USE_GPS && GPS_FAKE)
-#error							\
-    "Cannot use fake GPS without USE_GPS enabled!"	\
-    "Edit src/CMakelists.txt to enable USE_GPS"
-#endif // (!USE_GPS && GPS_FAKE)
 
 #define KALMAN_BIAS       1 // Use kalman estimation of acc bias
 
 #define CTRL_INTEGRAL     0 // Use PI control
-#define FULL_CONTROL      1 // Control 12 states
+#define FULL_CONTROL      0 // Control 12 states
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 // Do NOT edit the following lines!
@@ -29,6 +22,17 @@
 #define STATES_CONTROLLED     8
 #define STATES_INT_CONTROLLED 2
 #endif
+
+#if (!USE_GPS && GPS_FAKE)
+#error							\
+    "Cannot use fake GPS without USE_GPS enabled!"	\
+    "Edit src/CMakelists.txt to enable USE_GPS"
+#endif // (!USE_GPS && GPS_FAKE)
+
+#if (!USE_GPS && FULL_CONTROL)
+#error						\
+    "Cannot use FULL_CONTROL without USE_GPS!"
+#endif // (!USE_GPS && FULL_CONTROL)
 
 #if KALMAN_BIAS
 #define STATE_BIAS        3
