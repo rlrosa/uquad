d2412a28e66bd3d4b96751be444411be9992af9d
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-04-30 20:50:21 -0300

    diff entre rampa y rampa + prolijo (no hay cambio conceptual)

--- src/main/main.c
+++ src/main/main.c
@@ -74,18 +74,18 @@
 #define MOT_UPDATE_T MOT_UPDATE_MAX_US
 
 /// Global structs
-static imu_t *imu;
-static kalman_io_t *kalman;
-static uquad_mot_t *mot;
-static io_t *io;
-static ctrl_t *ctrl;
-static path_planner_t *pp;
+static imu_t *imu          = NULL;
+static kalman_io_t *kalman = NULL;
+static uquad_mot_t *mot    = NULL;
+static io_t *io            = NULL;
+static ctrl_t *ctrl        = NULL;
+static path_planner_t *pp  = NULL;
 #if USE_GPS && !GPS_ZERO
-static gps_t *gps;
+static gps_t *gps          = NULL;
 #endif
 /// Global var
-uquad_mat_t *w, *wt;
-uquad_mat_t *x;
+uquad_mat_t *w = NULL, *wt = NULL, *w_ramp = NULL;
+uquad_mat_t *x = NULL;
 imu_data_t imu_data;
 struct timeval tv_start = {0,0};
 uquad_bool_t
@@ -220,6 +220,7 @@ void quit()
 
     /// Global vars
     uquad_mat_free(w);
+    uquad_mat_free(w_ramp);
     uquad_mat_free(wt);
     uquad_mat_free(x);
     uquad_mat_free(imu_data.acc);
@@ -620,11 +621,12 @@ int main(int argc, char *argv[]){
     /// Global vars
     w = uquad_mat_alloc(4,1);        // Current angular speed [rad/s]
     wt = uquad_mat_alloc(1,4);        // tranpose(w)
+    w_ramp = uquad_mat_alloc(4,1);
     x = uquad_mat_alloc(1,STATE_COUNT);   // State vector
     retval = imu_data_alloc(&imu_data);
     quit_if(retval);
 
-    if( x == NULL || w == NULL || wt == NULL)
+    if( x == NULL || w == NULL || wt == NULL || w_ramp == NULL)
     {
 	err_log("Cannot run without x or w, aborting...");
 	quit();
@@ -1057,7 +1059,7 @@ int main(int argc, char *argv[]){
 		pp->sp->x->m_full[SV_THETA] = imu_data.magn->m_full[2];
 		// Motor speed
 		for(i=0; i<MOT_C; ++i)
-		    pp->sp->w->m_full[i] = mot->w_min;
+		    w_ramp->m_full[i] = mot->w_min;
 	    }
 
 	    /**
@@ -1129,28 +1131,16 @@ int main(int argc, char *argv[]){
 		ts_error_wait = 0;
 	    }
 	}
-	if(runs_kalman > STARTUP_KALMAN)
-	{
-	    // use real w
-	    retval = uquad_kalman(kalman,
-				  mot->w_curr,
-				  &imu_data,
-				  tv_diff.tv_usec,
-				  mot->weight,
-				  gps_update?gps_dat:NULL);
-	    log_n_continue(retval,"Inertial Kalman update failed");
-	}
-	else
-	{
-	    // use w from setpoint
-	    retval = uquad_kalman(kalman,
-				  pp->sp->w,
-				  &imu_data,
-				  tv_diff.tv_usec,
-				  mot->weight,
-				  NULL);
-	    log_n_continue(retval,"Inertial Kalman update failed");
-	}
+	// use real w
+	retval = uquad_kalman(kalman,
+			      (runs_kalman > STARTUP_KALMAN)?
+			      mot->w_curr:w_ramp,
+			      &imu_data,
+			      tv_diff.tv_usec,
+			      mot->weight,
+			      gps_update?gps_dat:NULL);
+	log_n_continue(retval,"Inertial Kalman update failed");
+
 	/// Mark time when we run Kalman
 	tv_last_kalman = tv_tmp;
 #if USE_GPS
@@ -1244,10 +1234,13 @@ int main(int argc, char *argv[]){
 		 * balance.
 		 */
 		for(i = 0; i < MOT_C; ++i)
+		{
+		    w_ramp->m_full[i] = w->m_full[i];
 		    w->m_full[i] = uquad_max(mot->w_min,
 					     w->m_full[i] - (STARTUP_KALMAN - runs_kalman)
 					     *((mot->w_hover - mot->w_min)/STARTUP_KALMAN)
 					     );
+		}
 	    }
 	    retval = mot_set_vel_rads(mot, w, false);
 	    log_n_continue(retval,"Failed to set motor speed!");
