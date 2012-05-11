662feae9d4e0ab437a1c4874922daed2761c5295
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-05-11 13:27:57 -0300

    borro de todo

--- print_like_uquad_mat.m
+++
@@ -1,8 +0,0 @@
-function [] = print_like_uquad_mat(struct,nombre_matriz,A)
-[M,N] = size(A);
-
-for i=1:M
-    for j=1:N
-        fprintf('%s -> %s -> m[%d][%d] = %0.20f;\n',struct,nombre_matriz,i-1,j-1,A(i,j));
-    end
-end
--- tests/Motores/data/monod.m
+++
@@ -1,5 +0,0 @@
-function f = monod(x);
-i2c=evalin('base','gi2c');
-w=evalin('base','gw');
- 
-f= w-x(1)*i2c./(x(2)*i2c+x(3));
