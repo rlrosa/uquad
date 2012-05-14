4e4267088f3e9a146cb51562b62cffa4a3667bcc
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-05-14 19:23:09 -0300

    check net como lib

--- src/check_net/client.c
+++ src/check_net/client.c
@@ -1,160 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
-#include <mot_control.h> // for KILL_MOTOR_CMD
-#include <uquad_types.h>
-#include <uquad_aux_time.h>
-#include <uquad_aux_io.h>
+#include <uquad_check_net.h>
 
 #define USAGE "usage:\n\t./client <host IP> <port>\n"
-
-static int sockfd = -1;
-
-void quit()
-{
-    if(sockfd > 0)
-	close(sockfd);
-    exit(-1);
-}
 
 int main(int argc, char *argv[])
 {
     int
 	portno,
-	n,
-	retval,
-	kill_retries = 0;
-    uquad_bool_t
-	read_ok = false,
-	server_ok = false;
-    struct sockaddr_in
-	servaddr;
-    struct hostent
-	*server;
-    struct timeval
-	tv_out,
-	tv_sent,
-	tv_diff,
-	tv_tmp;
-    socklen_t
-	len;
-    char
-	buff_i[CHECK_NET_MSG_LEN],
-	buff_o[CHECK_NET_MSG_LEN] = CHECK_NET_PING;
-
-    tv_out.tv_usec = 0;
-    tv_out.tv_sec  = CHECK_NET_TO_S;
+	retval;
 
     if (argc < 3)
     {
-	quit_log_if(ERROR_INVALID_ARG, USAGE);
+	err_check(ERROR_INVALID_ARG, USAGE);
     }
 
     portno = atoi(argv[2]);
 
-    server = gethostbyname(argv[1]);
-    if(server == NULL)
-    {
-	err_log_str("ERROR, no such host", argv[1]);
-	quit();
-    }
-
-    sockfd=socket(AF_INET,SOCK_DGRAM,0);
-
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
-    servaddr.sin_port=htons(portno);
-
-    bzero(buff_i,CHECK_NET_MSG_LEN);
-    while(1)
-    {
-	/// ping server
-	n = sendto(sockfd,buff_o,CHECK_NET_MSG_LEN,0,
-		   (struct sockaddr *)&servaddr,sizeof(servaddr));
-	if (n < 0)
-	{
-	    err_log_stderr("sendto()");
-	    goto kill_motors;
-	}
-	bzero(buff_i,CHECK_NET_MSG_LEN);
-	gettimeofday(&tv_sent, NULL);
-	/// give server some time to answer
-	sleep_ms(CHECK_NET_RETRY_MS);
-
-	/// wait for ack
-	while(1)
-	{
-	    len = sizeof(servaddr);
-	    retval = check_io_locks(sockfd, NULL, &read_ok, NULL);
-	    quit_if(retval);
-	    if(read_ok)
-	    {
-		n=recvfrom(sockfd,buff_i,CHECK_NET_MSG_LEN,
-			   0,(struct sockaddr *)&servaddr,&len);
-		if (n < 0)
-		{
-		    err_log_stderr("recvfrom()");
-		    goto kill_motors;
-		}
-		if (n == CHECK_NET_MSG_LEN &&
-		    (strcmp(buff_i,CHECK_NET_ACK) == 0))
-		{
-		    // ack received
-		    server_ok = true;
-		    sleep_ms(CHECK_NET_MSG_T_MS);
-		    break;
-		}
-		sleep_ms(CHECK_NET_RETRY_MS);
-	    }
-
-	    /// check timeout
-	    gettimeofday(&tv_tmp, NULL);
-	    (void) uquad_timeval_substract(&tv_diff, tv_tmp, tv_sent);
-	    retval = uquad_timeval_substract(&tv_diff, tv_out, tv_diff);
-	    if(retval < 0)
-	    {
-		/// timed out, game over
-		kill_motors:
-		if(kill_retries == 0)
-		{
-		    if(!server_ok)
-		    {
-			err_log("");
-			err_log("");
-			err_log("");
-			err_log("");
-			err_log("");
-			err_log("-- -- -- -- --");
-			err_log("-- -- -- -- -- -- -- -- -- --");
-			err_log("WARN: Will NOT run checknet, server never acked...");
-			err_log("-- -- -- -- -- -- -- -- -- --");
-			err_log("-- -- -- -- --");
-			err_log("");
-			err_log("");
-			err_log("");
-			err_log("");
-			err_log("");
-			quit();
-		    }
-		    err_log("check_net: Connection lost! Killing motors...");
-		}
-		retval = system(KILL_MOTOR_CMD);
-		if(retval < 0 && kill_retries++ < CHECK_NET_KILL_RETRIES)
-		{
-		    usleep(1000);
-		    goto kill_motors;
-		}
-		quit();
-	    }
-	    
-	}
-    }
+    retval = uquad_check_net_client(argv[1], portno);
+    err_check(retval, "client() failed!");
+    return retval;
 }
--- src/check_net/server.c
+++ src/check_net/server.c
@@ -1,96 +1,21 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#include <uquad_types.h>
-#include <uquad_error_codes.h>
-#include <uquad_aux_time.h>
+#include <uquad_check_net.h>
 
 #define USAGE "usage:\n\t./server portno\n"
-
-static int sockfd = -1, newsockfd = -1;
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
-
-void quit()
-{
-    if(newsockfd > 0)
-	close(newsockfd);
-    if(sockfd > 0)
-	close(sockfd);
-    exit(-1);
-}
 
 int main(int argc, char *argv[])
 {
      int
 	 portno,
-	 n;
-     char
-	 buff_o[CHECK_NET_MSG_LEN] = CHECK_NET_ACK,
-	 buff_i[CHECK_NET_MSG_LEN];
-     struct sockaddr_in servaddr, cliaddr;
-     socklen_t len;
+	 retval;
 
      if (argc < 2)
      {
-	 quit_log_if(ERROR_INVALID_ARG, USAGE);
+	 err_check(ERROR_INVALID_ARG, USAGE);
      }
      portno = atoi(argv[1]);
 
-     sockfd=socket(AF_INET,SOCK_DGRAM,0);
+     retval = uquad_check_net_server(portno);
+     err_check(retval, "server() failed!");
 
-     bzero(&servaddr,sizeof(servaddr));
-     servaddr.sin_family = AF_INET;
-     servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-     servaddr.sin_port=htons(portno);
-     if(bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
-     {
-	 err_log_stderr("bind()");
-	 quit();
-     }
-
-     while(1)
-     {
-	 len = sizeof(cliaddr);
-	 n = recvfrom(sockfd,buff_i,CHECK_NET_MSG_LEN,0,(struct sockaddr *)&cliaddr,&len);
-	 if (n < 0)
-	 {
-	     /// Something went wrong, die.
-	     err_log_stderr("recvfrom()");
-	     quit();
-	 }
-	 if(n == CHECK_NET_MSG_LEN &&
-	    (strcmp(buff_i,CHECK_NET_PING) == 0))
-	 {
-	     /// Got msg from client, ack to inform we're alive
-	     n = sendto(sockfd,buff_o,CHECK_NET_MSG_LEN,
-			0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-	     if (n < 0)
-	     {
-		 err_log_stderr("sendto()");
-		 quit();
-	     }
-	     /**
-	      * Client pings at a CHECK_NET_MSG_T_MS rate.
-	      * We'll read more often, since timing is not
-	      * a problem on the server side (PC)
-	      */
-	     sleep_ms(CHECK_NET_MSG_T_MS >> 1);
-	 }
-	 /// nothing new...
-	 usleep(100);
-	 continue;
-
-     }
+     exit(retval);
 }
