85d2c0f9da040b188998078bc78cc75009253f1f
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-05-15 16:05:56 -0300

    check_net opcion tcp/udp

--- src/check_net/server.c
+++ src/check_net/server.c
@@ -1,20 +1,26 @@
 #include <uquad_check_net.h>
 
-#define USAGE "usage:\n\t./server portno\n"
+#define USAGE "usage:\n\t./server <portno> [<TCP/!UDP>]\n"
 
 int main(int argc, char *argv[])
 {
      int
 	 portno,
 	 retval;
+     uquad_bool_t
+	 tcp = true;
 
-     if (argc < 2)
+     if (argc < 2 || argc > 3)
      {
 	 err_check(ERROR_INVALID_ARG, USAGE);
      }
+     if (argc > 2)
+     {
+	 tcp = atoi(argv[2]);
+     }
      portno = atoi(argv[1]);
 
-     retval = uquad_check_net_server(portno);
+     retval = uquad_check_net_server(portno, tcp);
      err_check(retval, "server() failed!");
 
      exit(retval);
--- src/check_net/uquad_check_net.h
+++ src/check_net/uquad_check_net.h
@@ -26,19 +26,24 @@
  * it will not (except if an error occurs) stop running.
  *
  * @param portno Port on which server should listen
+ * @param tcp Use a connection oriented socket (TCP)
  *
  * @return error code
  */
-int uquad_check_net_server(int portno);
+int uquad_check_net_server(int portno, uquad_bool_t tcp);
 
 /**
- * 
+ * Will launch a client that will periodically ping a server.
+ * If does not recieve any answers from the server, it will die
+ * without doing anything. If at least 1 ack is received, then
+ * if connection is lost, it will kill the motor driver.
  *
- * @param hostIP 
- * @param portno 
+ * @param hostIP IP of the host where the server is running.
+ * @param portno Port used by the server.
+ * @param tcp Use a connection oriented socket (TCP)
  *
  * @return error code
  */
-int uquad_check_net_client(const char *hostIP, int portno);
+int uquad_check_net_client(const char *hostIP, int portno, uquad_bool_t tcp);
 
 #endif // UQUAD_CHECK_NET_H
