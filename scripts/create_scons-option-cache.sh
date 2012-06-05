7911ed4eb14e563cafbc46587005a0efe0544278
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2012-06-05 19:00:28 -0300

    espacios y boludeces

--- scripts/create_scons-option-cache.sh
+++ scripts/create_scons-option-cache.sh
@@ -6,6 +6,7 @@ echo "shared = False" >> .scons-option-cache
 echo "libgpsmm = False" >>  .scons-option-cache
 echo "libQgpsmm = False" >> .scons-option-cache
 echo "python = False" >> .scons-option-cache
+
 echo prefix = "'"${OE_BEAGLE_TOOLCHAIN}"'" >> .scons-option-cache
 echo "target = 'arm-angstrom-linux-gnueabi'" >> .scons-option-cache
 echo sysroot = "'"${OE_BUILD_TMPDIR}-eglibc/sysroots/beagleboard/"'" >> .scons-option-cache
